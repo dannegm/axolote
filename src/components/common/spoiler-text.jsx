4fffff76345dcb31290e35f8d50998f5de7a79a7
dannegm <im@danielgarcia.me>
2025-01-02 23:09:01 -0600

    Agregando rich text, efectos y demas

--- src/components/common/spoiler-text.jsx
+++ src/components/common/spoiler-text.jsx
@@ -1,26 +1,9 @@
 'use client';
 
-import { createContext, useContext, useState } from 'react';
+import { useState } from 'react';
 import useSound from 'use-sound';
 
 import { cn } from '@/helpers/utils';
-
-const SpoilerContext = createContext();
-
-const SpoilerWord = ({ children }) => {
-    const { hidden, toggleSpoiler } = useContext(SpoilerContext);
-    return (
-        <span
-            className={cn(
-                'cursor-pointer rounded-md px-1 box-decoration-clone bg-purple-400 text-white transition-all duration-500',
-                { 'blur-sm': hidden },
-            )}
-            onClick={toggleSpoiler}
-        >
-            {children}
-        </span>
-    );
-};
 
 export default function SpoilerText({ children }) {
     const [playHush] = useSound('./sounds/shush.mp3');
@@ -30,19 +13,15 @@ export default function SpoilerText({ children }) {
         setHidden(!hidden);
         playHush();
     };
-
-    const words = children
-        .trim()
-        .split(' ')
-        .map((word, index) => ({ key: `${word}_${index}`, word }));
-
     return (
-        <SpoilerContext.Provider value={{ hidden, toggleSpoiler }}>
-            <span className='inline-flex flex-row justify-center flex-wrap gap-1'>
-                {words.map(item => (
-                    <SpoilerWord key={item.key}>{item.word}</SpoilerWord>
-                ))}
-            </span>
-        </SpoilerContext.Provider>
+        <span
+            className={cn(
+                'cursor-pointer px-1 font-extrabold text-purple-500 transition-all duration-500',
+                { 'blur-md bg-purple-500 rounded-md box-decoration-clone': hidden },
+            )}
+            onClick={toggleSpoiler}
+        >
+            {children}
+        </span>
     );
 }
