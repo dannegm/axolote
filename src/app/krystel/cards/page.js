ffa82e89b5134135733e740e538f8e6dfe893b75
dannegm <im@danielgarcia.me>
2025-01-25 17:40:05 -0600

    Update paragraph styling in Cards component to include horizontal margins.

--- src/app/krystel/cards/page.js
+++ src/app/krystel/cards/page.js
@@ -14,7 +14,7 @@ export default function Cards() {
             <main className='px-4 md:p-0 w-full md:w-3/4 lg:w-4/6 xl:w-1/2 mx-auto border-t-8 border-gray-300'>
                 <div className='flex flex-col gap-2 text-center my-6'>
                     <h1 className='font-pacifico text-3xl text-center text-indigo-950'>Krystel,</h1>
-                    <p className='text-center text-sm text-slate-500 text-balance'>
+                    <p className='text-center text-sm text-slate-500 mx-8'>
                         Aquí podrás encontrar todas las tarjetitas que he creado.
                     </p>
                 </div>
