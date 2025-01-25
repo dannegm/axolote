50a741fdd4eb302a3c4cf1d490b03f8662b28f5c
dannegm <im@danielgarcia.me>
2025-01-24 20:21:39 -0600

    Add onDelete prop to DeleteLogButton for handling deletion state in LogItem component.

--- src/app/logs/components/delete-log-button.jsx
+++ src/app/logs/components/delete-log-button.jsx
@@ -3,13 +3,19 @@
 import useDeleteLogAction from '@/modules/krystel/hooks/use-detelete-log-action';
 import { X } from 'lucide-react';
 
-export default function DeleteLogButton({ id }) {
+export default function DeleteLogButton({ id, onDelete }) {
     const deleteLog = useDeleteLogAction();
+
+    const handleDelete = () => {
+        onDelete?.();
+        deleteLog(id);
+    };
+
     return (
         <button
             type='button'
             className='cursor-pointer bg-gray-200 text-gray-500 hover:scale-110 hover:bg-slate-300 hover:shadow-sm active:scale-95 rounded-full h-5 w-5 inline-flex items-center justify-center transition-all'
-            onClick={() => deleteLog(id)}
+            onClick={handleDelete}
         >
             <X size='0.85rem' strokeWidth='4px' />
         </button>
--- src/app/logs/components/log-item.jsx
+++ src/app/logs/components/log-item.jsx
@@ -6,10 +6,20 @@ import Badge from '@/modules/core/components/tremor/badge';
 import GiftCardPreview from '@/modules/krystel/components/common/gift-card-preview';
 import { UserAgentInfo } from './user-agent-info';
 import DeleteLogButton from './delete-log-button';
+import { useState } from 'react';
+import { cn } from '@/modules/core/helpers/utils';
 
 export default function LogItem({ item }) {
+    const [deleting, setDeleting] = useState(false);
     return (
-        <div className='flex flex-col md:flex-row gap-4 lg:gap-8 py-4 border-t border-gray-200 text-sm'>
+        <div
+            className={cn(
+                'flex flex-col md:flex-row gap-4 lg:gap-8 py-4 border-t border-gray-200 text-sm transition-all duration-150 opacity-100',
+                {
+                    'opacity-0': deleting,
+                },
+            )}
+        >
             <div className='flex flex-col gap-2 items-start md:w-3/6'>
                 <span>{item.ip_location}</span>
                 <Badge variant='neutral'>{item.ip}</Badge>
@@ -19,7 +29,7 @@ export default function LogItem({ item }) {
             <div className='flex flex-col gap-4 items-start md:w-full'>
                 <div className='flex w-full gap-2 justify-between'>
                     <Badge variant={item.type}>{item.type}</Badge>
-                    <DeleteLogButton id={item.id} />
+                    <DeleteLogButton id={item.id} onDelete={() => setDeleting(true)} />
                 </div>
                 <span className='flex-none w-full'>
                     <GiftCardPreview quote={item.quotes.quote} code={item.metadata?.code} />
