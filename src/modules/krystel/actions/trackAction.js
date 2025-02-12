831863940efed07b95178bac68430c1dbeaca153
dannegm <im@danielgarcia.me>
2025-02-11 20:27:40 -0600

    Update trackAction to include session ID and use local storage for session ID management in useTrackAction hook.

--- src/modules/krystel/actions/trackAction.js
+++ src/modules/krystel/actions/trackAction.js
@@ -1,8 +1,8 @@
 'use client';
 const HOSTNAME = 'https://endpoints.hckr.mx/quotes/krystel';
 
-export const trackAction = async ({ referrer, userAgent = 'unknown' }) => {
-    const url = `${HOSTNAME}/track?ua=${userAgent}`;
+export const trackAction = async ({ sid, referrer, userAgent = 'unknown' }) => {
+    const url = `${HOSTNAME}/track?sid=${sid}&ua=${userAgent}`;
     await fetch(url, {
         method: 'POST',
         credentials: 'include',
--- src/modules/krystel/hooks/use-track-action.js
+++ src/modules/krystel/hooks/use-track-action.js
@@ -1,10 +1,13 @@
 'use client';
 import { useEffect } from 'react';
+import { nanoid } from 'nanoid';
 import { useQueryState, parseAsBoolean } from 'nuqs';
+import { useLocalStorage } from '@uidotdev/usehooks';
 import { useMutation } from '@tanstack/react-query';
 import { trackAction } from '@/modules/krystel/actions/trackAction';
 
 export default function useTrackAction() {
+    const [sid] = useLocalStorage('sid', nanoid());
     const [skipActions] = useQueryState('skip-actions', parseAsBoolean.withDefault(false));
 
     const mutation = useMutation({
@@ -14,9 +17,10 @@ export default function useTrackAction() {
     useEffect(() => {
         if (!skipActions) {
             mutation.mutate({
+                sid,
                 userAgent: window.navigator.userAgent,
                 referrer: document.referrer,
             });
         }
-    }, []);
+    }, [sid]);
 }
