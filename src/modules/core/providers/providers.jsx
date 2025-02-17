2cca6c01002a51d3400a21cae96199fb7bf307de
dannegm <im@danielgarcia.me>
2025-02-17 00:54:27 -0700

    Add Vercel Analytics integration to the project and update package.json and yarn.lock

--- src/modules/core/providers/providers.jsx
+++ src/modules/core/providers/providers.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { Suspense } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NuqsAdapter } from 'nuqs/adapters/next/app';
 import ToastProvider from './toast-provider';
@@ -10,6 +11,7 @@ export default function Providers({ children }) {
     return (
         <NuqsAdapter>
             <QueryClientProvider client={queryClient}>
+                <Analytics />
                 <ToastProvider>
                     <Suspense>{children}</Suspense>
                 </ToastProvider>
