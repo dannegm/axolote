5629bce8874ec8f1cc603416463fff95035338f2
dannegm <im@danielgarcia.me>
2025-02-16 17:47:57 -0700

    Update z-index of permission request button in DevicePerspectiveCard component from 'z-max' to 'z-50'.

--- src/modules/krystel/components/common/device-perspective-card.jsx
+++ src/modules/krystel/components/common/device-perspective-card.jsx
@@ -75,7 +75,7 @@ export default function DevicePerspectiveCard({ children, className = '' }) {
             {!permissionGranted && (
                 <button
                     onClick={requestPermission}
-                    className='group absolute bottom-8 right-8 z-max'
+                    className='group absolute bottom-8 right-8 z-50'
                 >
                     <div
                         className='relative block bg-cyan-300 text-cyan-800 text-sm py-1 px-2 font-delius rounded-full shadow-lg group-hover:shadow-xl group-active:shadow-md'
