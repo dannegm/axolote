f79fb5cce27643f725576dff19232d442a147d98
dannegm <im@danielgarcia.me>
2025-02-18 04:43:04 -0700

    uwu mode for cards page

--- src/app/krystel/cards/components/cards-list.jsx
+++ src/app/krystel/cards/components/cards-list.jsx
@@ -1,11 +1,34 @@
+import { useQueryState, parseAsBoolean } from 'nuqs';
+import ClientOnly from '@/modules/core/components/common/client-only';
 import CardItem from './card-item';
 
 export default function CardsList({ data = [] }) {
+    const [uwu] = useQueryState('uwu', parseAsBoolean.withDefault(false));
+
     return (
-        <div className='grid grid-flow-row pb-16'>
-            {data.map(item => (
-                <CardItem key={`card-item-${item.id}`} item={item} />
-            ))}
-        </div>
+        <ClientOnly>
+            <main className='px-4 md:p-0 w-full md:w-3/4 lg:w-4/6 xl:w-1/2 mx-auto border-t-8 border-gray-300'>
+                <div className='flex flex-col gap-2 text-center my-6'>
+                    {!uwu ? (
+                        <h1 className='font-pacifico text-3xl text-center text-indigo-950'>
+                            Krystel,
+                        </h1>
+                    ) : (
+                        <figure className='flex-center -mt-4 text-center'>
+                            <img className='h-24' src='/krystel-uwu.png' alt='Krystel' />
+                        </figure>
+                    )}
+                    <p className='text-center text-sm text-slate-500 mx-8'>
+                        Aquí podrás encontrar todas las tarjetitas que he creado.
+                    </p>
+                </div>
+
+                <div className='grid grid-flow-row pb-16'>
+                    {data.map(item => (
+                        <CardItem key={`card-item-${item.id}`} item={item} />
+                    ))}
+                </div>
+            </main>
+        </ClientOnly>
     );
 }
--- src/app/krystel/cards/page.js
+++ src/app/krystel/cards/page.js
@@ -15,16 +15,7 @@ export default function Cards() {
             <Menu />
             <TrackAction />
             <PageViewAction page='cards' />
-
-            <main className='px-4 md:p-0 w-full md:w-3/4 lg:w-4/6 xl:w-1/2 mx-auto border-t-8 border-gray-300'>
-                <div className='flex flex-col gap-2 text-center my-6'>
-                    <h1 className='font-pacifico text-3xl text-center text-indigo-950'>Krystel,</h1>
-                    <p className='text-center text-sm text-slate-500 mx-8'>
-                        Aquí podrás encontrar todas las tarjetitas que he creado.
-                    </p>
-                </div>
-                <CardsLoader />
-            </main>
+            <CardsLoader />
         </>
     );
 }
