aa64b6eebe51f3800115af3a83302378da875d6f
dannegm <im@danielgarcia.me>
2025-02-27 22:37:25 -0600

    Add FingerprintJS for user tracking and replace session ID with fingerprint in tracking actions.

--- src/modules/krystel/hooks/use-track-action.js
+++ src/modules/krystel/hooks/use-track-action.js
@@ -1,16 +1,32 @@
 'use client';
 import { useEffect } from 'react';
-import { nanoid } from 'nanoid';
 import { useQueryState, parseAsBoolean } from 'nuqs';
 import { useMutation } from '@tanstack/react-query';
+import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 import useSettings from '@/modules/core/hooks/use-settings';
 import useLocalStorage from '@/modules/core/hooks/use-local-storage';
 import { trackAction } from '@/modules/krystel/actions/trackAction';
 
+const useFingerprint = () => {
+    const [fingerprint, setFingerprint] = useLocalStorage('fingerprint', null);
+
+    useEffect(() => {
+        if (fingerprint) return;
+
+        (async () => {
+            const fp = await FingerprintJS.load();
+            const result = await fp.get();
+            setFingerprint(result.visitorId);
+        })();
+    }, []);
+
+    return fingerprint;
+};
+
 export default function useTrackAction() {
     if (typeof window === 'undefined') return;
-    const [sid] = useLocalStorage('sid', nanoid());
+    const fingerprint = useFingerprint();
     const [skipActionsSettings] = useSettings('settings:skip_actions', false);
     const [skipActions] = useQueryState('skip-actions', parseAsBoolean.withDefault(false));
 
@@ -22,7 +38,7 @@ export default function useTrackAction() {
         if (skipActions || skipActionsSettings) return;
 
         mutation.mutate({
-            sid,
+            sid: fingerprint,
             userAgent: window.navigator.userAgent,
             referrer: document.referrer,
         });
@@ -30,5 +46,5 @@ export default function useTrackAction() {
 
     useEffect(() => {
         registerSession();
-    }, [sid]);
+    }, [fingerprint]);
 }
