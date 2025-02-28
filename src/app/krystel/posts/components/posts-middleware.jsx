ef7ee0b189bfc192e4c62ee6dd4a75ea65f20ee8
dannegm <im@danielgarcia.me>
2025-02-27 19:04:03 -0600

    Simplify PostsMiddleware to always render PostsLoader, removing the condition for enabling posts section.

--- src/app/krystel/posts/components/posts-middleware.jsx
+++ src/app/krystel/posts/components/posts-middleware.jsx
@@ -5,6 +5,5 @@ import PostsCommingSoon from './posts-comming-soon';
 import PostsLoader from './posts-loader';
 
 export default function PostsMiddleware() {
-    const [enablePostsSection] = useSettings('settings:posts:enable', false);
-    return <>{enablePostsSection ? <PostsLoader /> : <PostsCommingSoon />}</>;
+    return <PostsLoader />;
 }
