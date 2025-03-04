290e3a602867ba4ba6749c7edf1f08a5dcecff5c
dannegm <im@danielgarcia.me>
2025-03-04 07:43:36 -0600

    Adjust padding in CardEditorConfigs component for improved layout consistency.

--- src/app/krystel/secrets/editor/components/card-editor-configs.jsx
+++ src/app/krystel/secrets/editor/components/card-editor-configs.jsx
@@ -116,12 +116,12 @@ export default function CardEditorConfigs({ ref, className, configs, setConfigs 
     return (
         <ScrollArea
             className={cn(
-                'w-[calc(100%+32px)] h-auto max-h-[320px] sm:max-h-[400px] overflow-scroll -mx-4 px-4 pb-4',
+                'w-[calc(100%+32px)] h-auto max-h-[320px] sm:max-h-[400px] overflow-scroll -mx-4 pb-4',
                 className,
             )}
             type='always'
         >
-            <div className={cn('flex flex-col gap-4')}>
+            <div className={cn('flex flex-col gap-4 mx-4')}>
                 {/* Letter Mode */}
                 <div className='flex flex-row justify-between items-center'>
                     <Label htmlFor='configs:letter'>Letter mode</Label>
