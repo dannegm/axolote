b921137e1862b34f19c80bc7a08135ddef48afd7
dannegm <im@danielgarcia.me>
2025-03-07 10:06:07 -0600

    Center the title and adjust image editor container margins in PostImage component.

--- src/modules/krystel/components/apps/post-image.jsx
+++ src/modules/krystel/components/apps/post-image.jsx
@@ -6,15 +6,15 @@ import Frame from './frame';
 
 export default function PostImage({ q }) {
     return (
-        <Frame className='w-full -my-4'>
-            <h2 className='text-lg mb-4'>{q}</h2>
+        <Frame className='-my-4'>
+            <h2 className='text-lg mb-4 text-center mx-auto'>{q}</h2>
             <PostEditorHandler type='image' defaultContext={q}>
                 {({ content, setContent }) => (
                     <ImageEditor
                         content={content}
                         setContent={setContent}
                         props={{
-                            className: 'w-[260px] h-[180px]',
+                            className: 'w-[260px] h-[180px] -mx-6',
                             classNames: {
                                 imageContainer: 'inset-0 translate-x-0 min-w-full',
                                 inputDescription: 'bg-white/60 focus:bg-white/90',
