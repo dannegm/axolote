ed87a84687762e37040aaa2553427f4034ba5ee7
dannegm <im@danielgarcia.me>
2025-03-17 05:47:45 -0600

    Remove unused None component from PerspectiveCard file.

--- src/modules/krystel/components/common/perspective-card.jsx
+++ src/modules/krystel/components/common/perspective-card.jsx
@@ -4,8 +4,6 @@ import withResponsive from '@/modules/krystel/hocs/withResponsive';
 import MousePerspectiveCard from './mouse-perspective-card';
 import DevicePerspectiveCard from './device-perspective-card';
 
-const None = ({ children }) => <>{children}</>;
-
 const PerspectiveCard = withResponsive({
     mobile: DevicePerspectiveCard,
     default: MousePerspectiveCard,
