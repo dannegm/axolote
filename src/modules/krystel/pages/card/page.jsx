5c5d629657c31ddacc127ce17392537e40cff4a7
dannegm <im@danielgarcia.me>
2025-03-26 19:25:54 -0600

    de vuelta a la normalidad

--- src/modules/krystel/pages/card/page.jsx
+++ src/modules/krystel/pages/card/page.jsx
@@ -1,13 +1,10 @@
 import Layout from '@/modules/krystel/components/layout/layout';
-import TrackAction from '../../components/common/track-action';
-import PageViewAction from '../../components/common/page-view-action';
+import QuoteLoader from './components/quote-loader';
 
 export default function Page() {
     return (
-        <Layout title='Test'>
-            <TrackAction />
-            <PageViewAction page='main' />
-            <h1>Tú no eres Krys.</h1>
+        <Layout>
+            <QuoteLoader />
         </Layout>
     );
 }
