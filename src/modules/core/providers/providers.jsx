27169581f8df4d7374641f09d7a4b1f956090fcf
dannegm <im@danielgarcia.me>
2025-04-21 20:39:10 -0600

    Add Umami configuration to environment and update dependencies in package.json and yarn.lock

--- src/modules/core/providers/providers.jsx
+++ src/modules/core/providers/providers.jsx
@@ -1,15 +1,18 @@
 import { Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NuqsAdapter } from 'nuqs/adapters/react';
+import { TrackersProvider } from './trackers-provider';
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }) {
     return (
         <NuqsAdapter>
-            <QueryClientProvider client={queryClient}>
-                <Suspense>{children}</Suspense>
-            </QueryClientProvider>
+            <TrackersProvider>
+                <QueryClientProvider client={queryClient}>
+                    <Suspense>{children}</Suspense>
+                </QueryClientProvider>
+            </TrackersProvider>
         </NuqsAdapter>
     );
 }
